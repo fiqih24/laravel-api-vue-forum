2bbd876208ee26bee052286f37dfcb53cd8f8f6a
fiqih24 <zhangfiq@gmail.com>
2019-02-08 13:16:16 +0700

    relation

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -2,9 +2,18 @@
 
 namespace App\Model;
 
-use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Database\Eloquent\Model;
+use App\User;
 class Question extends Model
 {
-    //
+    public function user(){
+      return $this->belongsTo(User::class);
+    }
+    public function replies(){
+      return $this->hasMany(Reply::class);
+    }
+    public function categories(){
+      return $this->belongsTo(Category::class);
+    }
 }
